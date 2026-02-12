8f0e1932accc16ec2f3edf5da5961092c80d3c0d
admFLEG <admFLEG@SMG.local>
2026-02-12 11:32:20 +0100

    first commit

---
+++ TRAMESTDTCPIPCLIENT/TRAMESTDTCPIPCLIENT/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(16, 209)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(543, 190)
+        Me.RichTextBox1.TabIndex = 6
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(99, 111)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(340, 69)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Connecter au Balance"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(215, 18)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(288, 37)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "192.168.245."
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(215, 65)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(288, 37)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.Text = "23"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(43, 22)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 31)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Adresse IP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(43, 68)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 31)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "PORT"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 15)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(544, 187)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(613, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ TRAMESTDTCPIPCLIENT/TRAMESTDTCPIPCLIENT/Form1.vb
@@ -0,0 +1,167 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Runtime.InteropServices
+Imports System.Threading
+Imports System.IO
+
+Public Class Form1
+    Dim _client As TcpClient
+    Dim lastReceivedTime As DateTime
+    Dim pingTimer As Timer
+    Dim ip As String
+    Dim port As Integer
+    Dim filePath As String
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Shared Function SetThreadExecutionState(esFlags As EXECUTION_STATE) As EXECUTION_STATE
+    End Function
+
+    <Flags()>
+    Private Enum EXECUTION_STATE As UInteger
+        ES_CONTINUOUS = &H80000000UI
+        ES_SYSTEM_REQUIRED = &H1
+        ES_AWAYMODE_REQUIRED = &H40
+    End Enum
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS Or EXECUTION_STATE.ES_SYSTEM_REQUIRED Or EXECUTION_STATE.ES_AWAYMODE_REQUIRED)
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS)
+        pingTimer?.Dispose()
+        _client?.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ip = TextBox1.Text
+        port = Integer.Parse(TextBox2.Text)
+        filePath = GetFilePathFromIP(ip)
+        CheckForIllegalCrossThreadCalls = False
+
+        Try
+            _client = New TcpClient(ip, port)
+            lastReceivedTime = DateTime.Now
+
+            MsgBox("Connexion réussie")
+
+            pingTimer = New Timer(AddressOf SendPing, Nothing, Timeout.Infinite, Timeout.Infinite)
+            ThreadPool.QueueUserWorkItem(AddressOf ReceiveLoop)
+
+            Button1.Text = "Balance connectée"
+            Button1.Enabled = False
+        Catch ex As Exception
+            MsgBox("Erreur de connexion : " & ex.Message)
+            ThreadPool.QueueUserWorkItem(AddressOf ReceiveLoop)
+        End Try
+    End Sub
+
+    Private Sub SendPing(state As Object)
+        Try
+            If _client Is Nothing OrElse Not _client.Connected Then
+                Reconnect()
+                Return
+            End If
+
+            If (DateTime.Now - lastReceivedTime).TotalSeconds > 10 Then
+                Dim ns As NetworkStream = _client.GetStream()
+                Dim pingMessage As Byte() = Encoding.ASCII.GetBytes("ping")
+                ns.Write(pingMessage, 0, pingMessage.Length)
+            Else
+                pingTimer.Change(Timeout.Infinite, Timeout.Infinite)
+            End If
+        Catch
+            Reconnect()
+        End Try
+    End Sub
+
+    Private Sub ReceiveLoop(state As Object)
+        While True
+            Try
+                If _client Is Nothing OrElse Not _client.Connected Then
+                    Reconnect()
+                End If
+
+                Dim ns As NetworkStream = _client.GetStream()
+                ns.ReadTimeout = 15000
+
+                Dim toReceive(1024) As Byte
+                Dim bytesRead As Integer = ns.Read(toReceive, 0, toReceive.Length)
+
+                If bytesRead > 0 Then
+                    lastReceivedTime = DateTime.Now
+                    pingTimer.Change(Timeout.Infinite, Timeout.Infinite)
+
+                    Dim txt As String = Encoding.ASCII.GetString(toReceive, 0, bytesRead).Trim()
+                    UpdateUI(txt)
+                    SaveToFile(txt)
+                End If
+
+                pingTimer.Change(10000, 5000)
+            Catch ex As IOException
+                Reconnect()
+            Catch ex As Exception
+                Reconnect()
+            End Try
+        End While
+    End Sub
+
+    Private Sub Reconnect()
+        Do
+            Try
+                _client?.Close()
+            Catch
+            End Try
+
+            Try
+                _client = New TcpClient(ip, port)
+                lastReceivedTime = DateTime.Now
+                Exit Do
+            Catch
+                Thread.Sleep(5000)
+            End Try
+        Loop
+    End Sub
+
+    Private Sub UpdateUI(txt As String)
+        If RichTextBox1.TextLength > 0 Then
+            RichTextBox1.AppendText(vbNewLine & txt)
+        Else
+            RichTextBox1.Text = txt
+        End If
+    End Sub
+
+    Private Sub SaveToFile(txt As String)
+        If Not String.IsNullOrWhiteSpace(txt) Then
+            Try
+                Using file As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(filePath, True)
+                    file.WriteLine(txt.Replace(" ", "").Replace(Environment.NewLine, ""))
+                End Using
+            Catch
+                ' Ignorer les erreurs de fichier
+            End Try
+        End If
+    End Sub
+
+    Private Function GetFilePathFromIP(ip As String) As String
+        Select Case ip
+            Case "192.168.245.111"
+                Return "C:\balance\906\1\export\article.txt"
+            Case "192.168.245.112"
+                Return "C:\balance\906\2\export\article.txt"
+            Case "192.168.92.111"
+                Return "C:\balance\905\1\export\article.txt"
+            Case "192.168.92.114"
+                Return "C:\balance\905\2\export\article.txt"
+            Case Else
+                Return "C:\balance\default\export\article.txt"
+        End Select
+    End Function
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    End Sub
+End Class
